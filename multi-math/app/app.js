0482f1e93b7a7233c1f5997d8078a3c53b5383cd
Ashutosh <Ashutosh>
2023-12-23 21:42:10 +0530

    Updated the version for webpack

---
+++ multi-math/app/app.js
@@ -0,0 +1,5 @@
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to Multi-Math! Starting new gaem...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
