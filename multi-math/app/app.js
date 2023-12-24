d819c795e2fece0faaccb0249c2a736e954f62e2
Ashutosh <Ashutosh>
2023-12-24 16:09:21 +0530

    Added type annotations to logPlayerName() func

--- multi-math/app/app.js
+++
@@ -1,5 +0,0 @@
-function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to Multi-Math! Starting new gaem...';
-}
-document.getElementById('startGame').addEventListener('click', startGame);
--- multi-math/app/app.ts
+++ multi-math/app/app.ts
@@ -1,6 +1,11 @@
 function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to Multi-Math! Starting new gaem...'
+    logPlayerName('Ashutosh');
+}
+
+function logPlayerName(name: string): void {
+    console.log(`New player ${name} starting the game`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
